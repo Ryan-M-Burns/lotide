15c6cf552cc7028efa66263bf1ec9a4cee3c87a1
Ryan Matthew Burns <ryan.m.burns123@gmail.com>
2022-09-22 00:09:26 +0000

    created framework and test values for findKeyByValue function

---
+++ findKeyByValue.js
@@ -0,0 +1,34 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${actual} === ${expected}`);
+  }
+};
+
+const findKeyByValue = function(objectToSearch, findKeyValue) {
+
+}
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+
+const darkAngelsCharactersByRank = {
+  supreme_grand_master: "Azrael",
+  chief_librarian: "Ezekiel",
+  master_interrogator_chaplain: "Asmodai",
+  grand_master_of_the_ravenwing: "Sammael",
+  grand_master_of_the_deathwing: "Belial"
+};
+
+assertEqual(findKeyByValue(darkAngelsCharactersByRank, "Azrael"), "supreme_grand_master");
+assertEqual(findKeyByValue(darkAngelsCharactersByRank, "Ezekiel"), "chief_librarian");
+assertEqual(findKeyByValue(darkAngelsCharactersByRank, "Asmodai"), "master_interrogator_chaplain");
+assertEqual(findKeyByValue(darkAngelsCharactersByRank, "Sammael"), "grand_master_of_the_ravenwing");
+assertEqual(findKeyByValue(darkAngelsCharactersByRank, "Belial"), "grand_master_of_the_deathwing");
