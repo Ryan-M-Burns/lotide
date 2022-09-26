affce9434f22bca76509f122cc77b903f86f839f
Ryan Matthew Burns <ryan.m.burns123@gmail.com>
2022-09-26 22:42:12 +0000

    updated index to reference all files in lotide

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
-const eqObjects = require('./eqObjects');
-const eqArrays = require('./eqArrays');
-const head = require('./head');
-const assertEqual = require('./assertEqual');
-const tail = require('./tail');
 const assertArraysEqual = require('./assertArraysEqual');
+const assertEqual = require('./assertEqual');
+const assertObjectsEqual = require('./assertObjectsEqual');
+const countLetters = require('./countLetters');
+const countOnly = require('./countOnly');
+const eqArrays = require('./eqArrays');
+const eqObjects = require('./eqObjects');
+const findKey = require('./findKey');
+const flatten = require('./flatten');
+const head = require('./head');
+const letterPositions = require('./letterPositions');
+const map = require('./map');
 const middle = require('./middle');
+const tail = require('./tail');
+const takeUntil = require('./takeUntil');
+const without = require('./without');
 
 module.exports = {
-  eqObjects,
-  eqArrays,
-  head,
-  assertEqual,
-  tail,
   assertArraysEqual,
-  middle
+  assertEqual,
+  assertObjectsEqual,
+  countLetters,
+  countOnly,
+  eqArrays,
+  eqObjects,
+  findKey,
+  flatten,
+  head,
+  letterPositions,
+  map,
+  middle,
+  tail,
+  takeUntil,
+  without
 };
