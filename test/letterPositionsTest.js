3cbb622f10163dbb537749a50ea07ab3c67bf792
Ryan Matthew Burns <ryan.m.burns123@gmail.com>
2022-09-27 02:20:10 +0000

    implimented letterPositionsTest cases

--- test/letterPositionsTest.js
+++ test/letterPositionsTest.js
@@ -1,6 +1,23 @@
-const testCase = letterPositions("hello");
+const chai = require('chai');
+const assert = require('chai').assert;
+const _ = require('../index');
 
-assertArraysEqual(testCase['h'], [1]);
-assertArraysEqual(testCase['e'], [2]);
-assertArraysEqual(testCase['l'], [3, 4]);
-assertArraysEqual(testCase['o'], [5]);
+describe('function letterPositions()', () => {
+
+  it("returns { 'l': 1 } if the input is l", () => {
+    const input = 'l';
+    const expected = { 'l': [1] };
+
+
+    assert.deepStrictEqual(_.letterPositions(input), expected);
+  });
+
+  it("returns { 'h': [1], 'e': [2], 'l': [3, 4], 'o': [5] } if the input is 'hello'", () => {
+    const input = 'hello';
+    const expected = { 'h': [1], 'e': [2], 'l': [3, 4], 'o': [5] };
+
+
+    assert.deepStrictEqual(_.letterPositions(input), expected);
+  });
+
+});
