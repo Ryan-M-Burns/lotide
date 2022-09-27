833ed732ad1ef3b74938f047b08450a79cc05610
Ryan Matthew Burns <ryan.m.burns123@gmail.com>
2022-09-27 02:29:56 +0000

    implement middleTest

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,8 +1,27 @@
+const chai = require('chai');
+const assert = require('chai').assert;
 const _ = require('../index');
 
-_.assertArraysEqual(_.middle([1]), []);
-_.assertArraysEqual(_.middle([1, 2]), []);
-_.assertArraysEqual(_.middle([1, 2, 3]), [2]);
-_.assertArraysEqual(_.middle([1, 2, 3, 4, 5]), [3]);
-_.assertArraysEqual(_.middle([1, 2, 3, 4]), [2, 3]);
-_.assertArraysEqual(_.middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+describe('function map()', () => {
+
+  it('returns an empty array when the length of array is less than 3', () => {
+    input = [1, 2];
+    expected = [];
+    
+    assert.deepStrictEqual(_.middle(input), expected);
+  });
+
+  it('returns an single array when the length of array is odd and 3 or more', () => {
+    input = [1, 2, 3];
+    expected = [2];
+    
+    assert.deepStrictEqual(_.middle(input), expected);
+  });
+
+  it('returns an array with 2 elements when the array is even and length is greater than 3', () => {
+    input = [1, 2, 3, 4];
+    expected = [2, 3];
+    
+    assert.deepStrictEqual(_.middle(input), expected);
+  });
+});
