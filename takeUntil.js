2b872caffe8cca4f84b311d36f7f2dcafccc4904
Ryan Matthew Burns <ryan.m.burns123@gmail.com>
2022-09-27 01:12:39 +0000

    converted both tail and take until to export

--- takeUntil.js
+++ takeUntil.js
@@ -1,32 +1,3 @@
-const assertArraysEqual = function(actual, expected) {
-
-  if (eqArrays(actual, expected)) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑 Assertion Failed: ${actual} === ${expected}`);
-  }
-
-};
-
-const eqArrays = function(arrayOne, arrayTwo) {
-
-  if (arrayOne.length !== arrayTwo.length) {
-    return false;
-  }
-
-  for (let index = 0; index < arrayOne.length; index++) {
-    if (arrayOne[index] !== arrayTwo[index]) {
-      return false;
-    }
-  }
-
-  return true;
-};
-
-// create a function takeUntil that takes in two parameters
-// 1. the Array to work with
-// 2. The callback
-// takeUntil will return a slice of the array until the callback function returns a truthy value
 const takeUntil = function(array, callback) {
   result = [];
 
@@ -42,13 +13,4 @@ const takeUntil = function(array, callback) {
   return result;
 };
 
-const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-const results1 = takeUntil(data1, x => x < 0); // => [ 1, 2, 5, 7, 2 ]
-console.log(results1);
-assertArraysEqual(results1, [1, 2, 5, 7, 2]);
-console.log('---');
-
-const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-const results2 = takeUntil(data2, x => x === ','); // => [ 'I\'ve', 'been', 'to', 'Hollywood' ]
-console.log(results2);
-assertArraysEqual(results2, ['I\'ve', 'been', 'to', 'Hollywood']);
+module.exports = takeUntil;
--- test/tailTest.js
+++ test/tailTest.js
@@ -4,3 +4,7 @@ _.assertEqual(_.tail([]).toString(), [].toString());
 _.assertEqual(_.tail(["Hello", "Lighthouse", "Labs"]).toString(), ["Lighthouse", "Labs"].toString());
 _.assertEqual(_.tail([1, 2, 3]).toString(), [2, 3].toString());
 _.assertEqual(_.tail([1]).toString(), [].toString());
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
