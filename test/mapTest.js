5844e18abf07bcf61186ff5fbebb63b1d3246603
Ryan Matthew Burns <ryan.m.burns123@gmail.com>
2022-09-27 02:25:32 +0000

    implimented mapTest

--- test/mapTest.js
+++ test/mapTest.js
@@ -1,9 +1,22 @@
-const words = ["ground", "control", "to", "major", "tom"];
-const numbers = [6, 7, 2, 5, 3];
-const results1 = map(words, word => word[0]);
-const results2 = map(words, word => word.length);
-const results3 = map(numbers, number => number * 3);
-assertArraysEqual(results1, ['g', 'c', 't', 'm', 't']);
-assertArraysEqual(results2, [6, 7, 2, 5, 3]);
-assertArraysEqual(results3, [18, 21, 6, 15, 9]);
+const chai = require('chai');
+const assert = require('chai').assert;
+const _ = require('../index');
 
+describe('function map()', () => {
+
+  it("returns ['g', 'c', 't', 'm', 't'] when the callback word => word[0] is called on ['ground', 'control', 'to', 'major', 'tom']", () => {
+    const words = ["ground", "control", "to", "major", "tom"];
+    const callback = word => word[0];
+    const expected = ['g', 'c', 't', 'm', 't'];
+
+    assert.deepStrictEqual(_.map(words, callback), expected);
+  });
+
+  it("returns [18, 21, 6, 15, 9] when the callback num => num * 3 is called on [6, 7, 2, 5, 3]", () => {
+    const numbers = [6, 7, 2, 5, 3];
+    const callback = num => num * 3;
+    const expected = [18, 21, 6, 15, 9];
+
+    assert.deepStrictEqual(_.map(numbers, callback), expected);
+  });
+});
